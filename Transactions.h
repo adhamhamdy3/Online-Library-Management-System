50446c003dbd0db6715f9810caebb4f9052503e5
Adham Hamdy <160431507+adhamhamdy3@users.noreply.github.com>
2024-09-12 19:13:26 +0300

    Update Transactions.h

--- Transactions.h
+++ Transactions.h
@@ -13,15 +13,18 @@ using namespace std;
 
 class Transactions {
 private:
-    map<int, LibraryUser> activeAccounts;
+    map<string, LibraryUser> activeAccounts;
+    map<string, int> emails;
 public:
     LibraryUser& createAccount(const LibraryUser&);
+    bool isFound(const string&, const string&);
     LibraryUser* Login(const string&, const string&);
-    string getUsername(const int&);
-    string getPassword(const int&);
-    void changeUsername(const string&, const string&, const string&, const int&);
-    void changePassword(const string&, const string&, const string&, const int&);
-//    void deleteAccount(const string&, const string&, const int&);
+    string getUsername(const string&, const string&, const int&);
+    string getPassword(const string&, const string&, const int&);
+    void changeUsername(const string&, const string&, const string&, const string&, const int&);
+    void changePassword(const string&, const string&, const string&, const string&, const int&);
+    void deleteAccount(const string&, const string&, const string&, const int&);
+    int activeAccountsNumber() const;
 };
 
 
