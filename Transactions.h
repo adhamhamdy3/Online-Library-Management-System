23af8fa547965301fbdc8bcb6f41f1f326539d17
Adham Hamdy <160431507+adhamhamdy3@users.noreply.github.com>
2024-09-12 20:13:19 +0300

    static members and methods - changed

--- Transactions.h
+++ Transactions.h
@@ -13,18 +13,18 @@ using namespace std;
 
 class Transactions {
 private:
-    map<string, LibraryUser> activeAccounts;
-    map<string, int> emails;
+    static map<string, LibraryUser> activeAccounts;
 public:
-    LibraryUser& createAccount(const LibraryUser&);
-    bool isFound(const string&, const string&);
-    LibraryUser* Login(const string&, const string&);
-    string getUsername(const string&, const string&, const int&);
-    string getPassword(const string&, const string&, const int&);
-    void changeUsername(const string&, const string&, const string&, const string&, const int&);
-    void changePassword(const string&, const string&, const string&, const string&, const int&);
-    void deleteAccount(const string&, const string&, const string&, const int&);
-    int activeAccountsNumber() const;
+    static map<string, int> emails;
+    static LibraryUser& createAccount(const LibraryUser&);
+    static bool isFound(const string&, const string&);
+    static LibraryUser* Login(const string&, const string&);
+    static string getUsername(const string&, const string&, const int&);
+    static string getPassword(const string&, const string&, const int&);
+    static void changeUsername(const string&, const string&, const string&, const string&, const int&);
+    static void changePassword(const string&, const string&, const string&, const string&, const int&);
+    static void deleteAccount(const string&, const string&, const string&, const int&);
+    static int activeAccountsNumber() ;
 };
 
 
